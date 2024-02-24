3e4d5772a25dc644e0630d32f7b08f1f0db16943
laetitialp <zlolita@gmail.com>
2024-02-24 10:10:53 +0100

    fix a bug

--- matlab/Vermeer3_ana.m
+++ matlab/Vermeer3_ana.m
@@ -10,7 +10,7 @@ nu        = 0.1 ;
 col       = 'b';
 ifig      = 8;
 %%%%  Condition Initiale
-sxx = -200e3;
+sxx = -400e3;
 syy = -100e3;
 sxy = 0 ;
 %%%%  CL
@@ -24,31 +24,32 @@ nstep = gamma_tot/(gamma_dot_xy*dt);
 end
 
 function [sigma_out,sigma_in,beta,alpha,epsilon_out,epsilon_in] = Vermeer3_ana_llp2013(phi,psi,nu,sxx,syy,sxy,G,nstep,gamma_dot_xy)
-beenhere = 0 
+beenhere = 0;
+%elasticity
+L       = 2*G*nu/(1-2*nu);
+D = [L+2*G L 0 ;L L+2*G 0; 0 0 G];
+% cohesion
 c= 0;
+% forcing 
 sigma_dot_yy =0;
 eps_dot_xx   = 0;
 gamma_dot_xy_in = gamma_dot_xy;
 gamma_dot_xy_out = gamma_dot_xy;
+%initialize arrays
 sigma_out   = [sxx,syy,sxy]';
 sigma_in   = [sxx,syy,sxy]';
-
 epsilon_out   = [0 0 0]';
-epsilon_in   = [0 0 0]';
-
-%   [exx, eyy, gammaxy]'
-
-L       = 2*G*nu/(1-2*nu);
-D = [L+2*G L 0 ;L L+2*G 0; 0 0 G];
-
+epsilon_in    = [0 0 0]';
+gamma_bulk    = 0; 
+% 
 theta      = 90;
 theta_out  = theta;
-beta       = pi/2 ; 
+
+beta       = pi/2; 
 alpha      = beta;
+
 for i = 1:nstep
-    
     M = D;
-   
     F = f(sigma_in(:,i),phi,c);
     if F>=0
     dFdsig = [(sin(phi)-sin(beta(i)))/2;...
@@ -57,21 +58,9 @@ for i = 1:nstep
     dGdsig = [(sin(psi)-sin(beta(i)))/2;...
               (sin(psi)+sin(beta(i)))/2;...
               cos(beta(i))];
-    
-    % dGdsig          = [ 1/2*cos2theta + 1/2*sind(psi);...
-    %                    -cos2theta/2 + sind(psi)/2;...
-    %                     sin2theta];
     a               = D*dGdsig;
-    
-    
-    
-    % dFdsig          = [ 1/2*cos2theta + 1/2*sind(phi);...
-    %                    -cos2theta/2 + 1/2*sind(phi);...
-    %                     sin2theta];
     b               = D*dFdsig;
-    
     d = dFdsig'*D*dGdsig;
-    
     M = D-1/d*a*b';
     end
     
@@ -88,25 +77,24 @@ for i = 1:nstep
         sigma_dot_xy_out = (-M(3,2)*M(2,3) + M(3,3)*M(2,2))/M(2,2)*gamma_dot_xy_out;
     else
         if beenhere == 0 
-            gamma_dot_xy_in=gamma_dot_xy_in*10;
+            gamma_dot_xy_in=gamma_dot_xy*10;
             beenhere = 1
-        end
-          detA      =  (sin(beta(i))-sin(phi))*(sin(beta(i))- sin(psi))/4;
-          Z1        = -2*(nu-1)*(-sin(alpha(i))*sin(phi)+cos(alpha(i)-beta(i)))*cos(beta(i));
-          Z2        = (sin(beta(i))-sin(phi))*(sin(beta(i))-sin(psi))*cos(alpha(i));
-          Gamma     = detA/(Z2+Z1);
-          alpha_dot = 2*G*Gamma * gamma_dot_xy_in; 
-          sigma_dot_xx_in = 4*alpha_dot*cos(alpha(i))*cos(beta(i))/(sin(beta(i))-sin(phi));
-          sigma_dot_xy_in = 2*alpha_dot*cos(alpha(i)); 
+        end  
+          Z1 = ((2*nu-1)*sin(beta(i))-sin(psi))*cos(beta(i));
+          Z2 =  (2*nu-1)*cos(beta(i))^2+ sin(beta(i))*(sin(phi)+sin(psi))- sin(phi)*sin(psi)- 1;
+          denom = (sin(beta(i))- sin(psi))* (sin(beta(i))- sin(phi))*cos(alpha(i));
+          Gamma = G*denom/Z2; 
+          alpha_dot= Gamma * gamma_dot_xy_in; 
 
-          Z1 = (1-2*nu)*sin(alpha(i))*sin(psi)*(sin(beta(i))-sin(phi));  
-          Z2 = (1-nu) *cos(alpha(i))*cos(beta(i))*(sin(beta(i))+sin(psi));
-          Z3 = sin(alpha(i))*sin(beta(i))*(sin(beta(i))-sin(phi));
-          eps_dot_yy_in = (Z1+Z2+Z3)/detA/2/G*alpha_dot
+          %%%% 
+          sigma_dot_xx_in = -2*alpha_dot*cos(beta(i))/(cos(alpha(i))*(sin(beta(i))-sin(phi))); 
+          sigma_dot_xy_in = -alpha_dot/cos(alpha(i));
+          eps_dot_yy_in   = Z1/denom/2/G*alpha_dot; 
 
-          eps_dot_yy_out   = -2*alpha_dot*nu*sin(alpha(i))/G;
-          sigma_dot_xx_out = 4*alpha_dot*sin(alpha(i));
-          sigma_dot_xy_out = sigma_dot_xy_in;
+          eps_dot_yy_out   = 0;
+          sigma_dot_xx_out = 0; 
+          sigma_dot_xy_out = -alpha_dot/cos(alpha(i));
+          gamma_dot_xy_out = -alpha_dot/G/cos(alpha(i));
      end
     % increment stress and strain 
     sigma_in(:,i+1)   = sigma_in(:,i)     +  [sigma_dot_xx_in; sigma_dot_yy; sigma_dot_xy_in];
@@ -114,6 +102,7 @@ for i = 1:nstep
     
     sigma_out(:,i+1)   = sigma_out(:,i)   +  [sigma_dot_xx_out; sigma_dot_yy; sigma_dot_xy_out];
     epsilon_out(:,i+1) = epsilon_out(:,i) +  [eps_dot_xx; eps_dot_yy_out; gamma_dot_xy_out];
+    gamma_bulk(i+1)    = gamma_bulk(i)    +  gamma_dot_xy; 
 
      % update alpha and beta + theta for post processing purpose 
     tau_star   = 1/2*sqrt((sigma_in(1,i+1)-sigma_in(2,i+1))^2+4*sigma_in(3,i+1)^2);
@@ -127,13 +116,13 @@ for i = 1:nstep
 end
 figure(8);clf;
 subplot(221);hold on;
-plot(epsilon_out(3,:),-sigma_in(3,:)./sigma_in(2,:),'r',epsilon_out(3,:),-sigma_out(3,:)./sigma_out(2,:),'b');title('sxy/syy');
+plot(gamma_bulk,-sigma_in(3,:)./sigma_in(2,:),'r',gamma_bulk,-sigma_out(3,:)./sigma_out(2,:),'b');title('sxy/syy');
 subplot(224);hold on;
-plot(epsilon_out(3,:),theta,'r',epsilon_out(3,:),theta_out,'b');title('theta');
+plot(gamma_bulk,theta,'r',gamma_bulk,theta_out,'b');title('theta');
 subplot(223);hold on;
-plot(epsilon_out(3,:),epsilon_in(2,:),'r',epsilon_out(3,:),epsilon_out(2,:),'b');title('epsilon yy');
+plot(gamma_bulk,epsilon_in(2,:),'r',gamma_bulk,epsilon_out(2,:),'b');title('epsilon yy');
 subplot(222);hold on;
-plot(epsilon_out(3,:),-sigma_in(1,:),'r',epsilon_out(3,:),-sigma_out(1,:),'b');title('sxx');
+plot(gamma_bulk,-sigma_in(1,:),'r',gamma_bulk,-sigma_out(1,:),'b');title('sxx');
 end
 
 function F = f(sigma,phi,c)
